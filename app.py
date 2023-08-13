abfb9cf4a9cd0effac6d5040cb5c0f463db55ec8
HARISYOUSAFZAI <HARISALI.510@GMAIL.COM>
2023-08-13 18:11:44 +0300

    changed the msg in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=["GET","POST"])
 def index():
-    return "Starting the AlgoTrade Project"
+    return "CI CD Pipeline has been established for Starting the AlgoTrade Project"
 
 if __name__ == "__main__":
     app.run(debug=True)
